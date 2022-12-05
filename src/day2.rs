8d4558d3efcf72b47a8837fd009a6a14a3b388f3
Ollivier Robert <roberto@keltia.net>
2022-12-05 19:19:44 +0100

    Factorise the two tables into one.

--- src/day2.rs
+++ src/day2.rs
@@ -58,3 +58,26 @@ pub fn solver_part2(input: &str)-> u32 {
         .sum1::<u32>()
         .unwrap()
 }
+
+#[aoc(day2, part2, one)]
+pub fn solver_part2_one(input: &str)-> u32 {
+    let all: HashMap<&str, u32> = HashMap::from([
+        ("A X", 0 + 3),
+        ("A Y", 3 + 1),
+        ("A Z", 6 + 2),
+        ("B X", 0 + 1),
+        ("B Y", 3 + 2),
+        ("B Z", 6 + 3),
+        ("C X", 0 + 2),
+        ("C Y", 3 + 3),
+        ("C Z", 6 + 1),
+    ]);
+
+    input
+        .lines()
+        .map(|line| {
+            all[line]
+        })
+        .sum1::<u32>()
+        .unwrap()
+}
