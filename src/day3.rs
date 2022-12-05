ed6da9fbcfae374ee1eadab883448524794bca78
Ollivier Robert <roberto@keltia.net>
2022-12-05 17:50:24 +0100

    Placeholder for both parts.

---
+++ src/day3.rs
@@ -0,0 +1,18 @@
+
+use aoc_runner_derive::{aoc, aoc_generator};
+
+#[aoc_generator(day3)]
+pub fn generator(input: &str) -> Vec<u8> {
+    unimplemented!()
+}
+
+#[aoc(day3,part1)]
+pub fn solve_part1(input: Vec<u8>) -> u32 {
+    unimplemented!()
+}
+
+#[aoc(day3,part2)]
+pub fn solve_part2(input: Vec<u8>) -> u32 {
+    unimplemented!()
+}
+
