d7f1b5cbafd2103bdda6a6b68af83fd0b223ef83
Ollivier Robert <roberto@keltia.net>
2022-12-06 01:34:54 +0100

    Add second solution with any().

--- src/day3.rs
+++ src/day3.rs
@@ -1,7 +1,8 @@
+use std::collections::HashSet;
 use aoc_runner_derive::aoc;
 use itertools::{Itertools, Either};
 
-#[aoc(day3,part1)]
+#[aoc(day3,part1,partition_map)]
 pub fn solve_part1(input: &str) -> u32 {
     input
         .lines()
@@ -27,6 +28,48 @@ pub fn solve_part1(input: &str) -> u32 {
                     }
                 });
             uniq[0] as u8
+        })
+        //
+        // Now for each character, find value and sum everything.
+        //
+        .map(|c| {
+            let r = match c as char {
+                // 97..122 => 1..16
+                'a'..='z' => {
+                    (c as u8) - 96
+                }
+                // 65..90 => 27..52
+                'A'..='Z' => {
+                    (c as u8) - 38
+                }
+                _ => panic!("impossible")
+            };
+            r as u32
+        }).sum1()
+        .unwrap()
+}
+
+/// Alternate version using a set and .any().  Cleaner but much slower.
+///
+#[aoc(day3,part1,any)]
+pub fn solve_part1_any(input: &str) -> u32 {
+    input
+        .lines()
+        //
+        // split at middle
+        //
+        .map(|line| {
+            let splt = line
+                .split_at(line.len() / 2);
+            (splt.0.to_string(), splt.1.to_string())
+        })
+        //
+        // Check which character is present in both
+        //
+        .map(|(l,r)| {
+            let set: HashSet<char> = l.chars().collect();
+            let v = r.chars().find(|c| set.contains(c));
+            v.unwrap()
         })
         //
         // Now for each character, find value and sum everything.
