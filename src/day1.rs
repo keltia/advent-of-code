024a1da999f639c3ce54f099542dcc77fc497252
Ollivier Robert <roberto@keltia.net>
2025-12-06 23:14:27 +0100

    Day 1, part 1.

--- src/day1.rs
+++ src/day1.rs
@@ -1,43 +1,58 @@
+use std::num::ParseIntError;
+
 use aoc_runner_derive::{aoc, aoc_generator};
-use itertools::Itertools;
+use eyre::Result;
+use nom::character::complete::{i16, one_of};
+use nom::combinator::map_res;
+use nom::{IResult, Parser};
 
-#[aoc(day1, part1, max)]
-pub fn solve_part1(input: &[u32]) -> u32 {
-    *input.iter().sum().unwrap()
+#[derive(Copy, Clone, Debug)]
+enum Rotation {
+    Left(i16),
+    Right(i16),
 }
 
-#[aoc(day1, part1, sort_last)]
-pub fn solve_part1_sort(input: &[u32]) -> u32 {
-    let list: Vec<_> = input.iter().sorted().collect();
-    *list[input.len() - 1]
-}
-
-#[aoc(day1, part2, fold)]
-pub fn solve_part2(input: &[u32]) -> u32 {
-    let list: Vec<_> = input.iter().sorted().collect();
-    let sum = [1, 2, 3]
-        .iter()
-        .map(|v| list[input.len() - *v])
-        .fold(0, |sum, &x| sum + x);
-    sum
-}
-
-#[aoc(day1, part2, sum)]
-pub fn solve_part2_sum(input: &[u32]) -> u32 {
-    let list: Vec<_> = input.iter().sorted().collect();
-    let sum = [1, 2, 3].iter().map(|v| list[input.len() - *v]).sum();
-    sum
+struct Input1 {
+    pub data: Vec<Rotation>,
 }
 
 #[aoc_generator(day1)]
-pub fn input_generator(input: &str) -> Vec<u32> {
-    let mut cal = 0;
-    let mut cnt = 0;
-    input
+fn input_generator(input: &str) -> Result<Input1> {
+    let input = input
         .lines()
-        .map(|l| {
-            l.as_bytes().iter().filter_(|e| e.)
-        })
-        .filter_map(|(n, sum)| if n == 0 { Some(sum) } else { None })
-        .collect()
+        .map(|l| parse_rotation(l).unwrap().1)
+        .collect::<Vec<Rotation>>();
+    Ok(Input1 { data: input })
+}
+
+#[aoc(day1, part1)]
+fn solve_part1(input: &Input1) -> u32 {
+    let mut cnt = 0;
+
+    input.data.iter().fold(50, |acc, r| {
+        let m = match r {
+            Rotation::Left(amount) => -(*amount),
+            Rotation::Right(amount) => *amount,
+        };
+        let res = (acc + m) % 100;
+        if res == 0 {
+            cnt += 1;
+        }
+        res
+    });
+    cnt
+}
+
+fn parse_rotation(input: &str) -> IResult<&str, Rotation> {
+    let r = |(dir, amount): (char, std::primitive::i16)| -> Result<Rotation, ParseIntError> {
+        match dir {
+            'L' => Ok(Rotation::Left(amount)),
+            'R' => Ok(Rotation::Right(amount)),
+            _ => unreachable!(
+                "Invalid rotation direction: '{}'. Expected 'L' or 'R'.",
+                dir
+            ),
+        }
+    };
+    map_res((one_of("LR"), i16), r).parse(input)
 }
