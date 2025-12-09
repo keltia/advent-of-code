174f0b08d7dde2f75ed2a446831ea43286c7c584
Ollivier Robert <roberto@keltia.net>
2025-12-09 16:43:12 +0100

    sample_part1 and sample_part2 a re both correc now.

--- src/day1.rs
+++ src/day1.rs
@@ -2,14 +2,14 @@ use std::num::ParseIntError;
 
 use aoc_runner_derive::{aoc, aoc_generator};
 use eyre::Result;
-use nom::character::complete::{one_of, usize};
+use nom::character::complete::{one_of, u32};
 use nom::combinator::map_res;
 use nom::{IResult, Parser};
 
 #[derive(Copy, Clone, Debug)]
 enum Move {
-    Left(usize),
-    Right(usize),
+    Left(u32),
+    Right(u32),
 }
 
 #[derive(Clone, Debug)]
@@ -22,14 +22,14 @@ struct Input1 {
 #[aoc_generator(day1)]
 fn input_generator(input: &str) -> Result<Input1> {
     fn parse_rotation(input: &str) -> IResult<&str, Move> {
-        let r = |(dir, amount): (char, std::primitive::usize)| -> Result<Move, ParseIntError> {
+        let r = |(dir, amount): (char, std::primitive::u32)| -> Result<Move, ParseIntError> {
             match dir {
                 'L' => Ok(Move::Left(amount)),
                 'R' => Ok(Move::Right(amount)),
                 _ => unreachable!("Invalid Move direction: '{}'. Expected 'L' or 'R'.", dir),
             }
         };
-        map_res((one_of("LR"), usize), r).parse(input)
+        map_res((one_of("LR"), u32), r).parse(input)
     }
 
     let input = input
@@ -42,8 +42,8 @@ fn input_generator(input: &str) -> Result<Input1> {
 // -----
 
 #[aoc(day1, part1)]
-fn solve_part1(input: &Input1) -> usize {
-    let mut cnt = 0usize;
+fn solve_part1(input: &Input1) -> u32 {
+    let mut cnt = 0u32;
 
     input.data.iter().fold(50, |acc, r| {
         let m = match r {
@@ -58,15 +58,157 @@ fn solve_part1(input: &Input1) -> usize {
     });
     cnt
 }
+
+// -----
+
+const SIZE: u32 = 100;
+
+#[derive(Clone, Debug)]
+struct Wheel(u32);
+
+#[derive(Clone, Debug)]
+struct Turn(u32);
+
+impl Turn {
+    pub fn val(self) -> u32 {
+        self.0
+    }
+}
+
+impl Wheel {
+    pub fn new(initial: u32) -> Self {
+        Self(initial)
+    }
+
+    pub fn pos(&self) -> u32 { self.0 }
+
+    pub fn left(&mut self, amount: u32) -> Turn {
+        // Number of full rotations
+        let p = amount / SIZE;
+        dbg!(&amount, &p);
+
+        // Remainder
+        let q = (amount % SIZE) as i32;
+        let res = (self.0 as i32) - q;
+        dbg!(&q, &res);
+
+        // Final result is 0, count one more.
+        if res == 0 {
+            self.0 = res as u32;
+            return Turn(p + 1)
+        }
+
+        // Store result
+        let mut fin = 0;
+        if res < 0 {
+            fin = if self.0 == 0 {
+                p
+            } else {
+                p + 1
+            };
+            self.0 = (SIZE as i32 + res) as u32;
+        } else {
+            self.0 = res as u32;
+            fin = p;
+        }
+        Turn(fin)
+    }
+
+    pub fn right(&mut self, amount: u32) -> Turn {
+        // Number of full rotations
+        let p = amount / SIZE;
+        dbg!(&amount, &p);
+
+        let mut fin = 0;
+
+        // Look at result
+        let q = amount % SIZE;
+        let res = self.0 + q;
+        dbg!(&q, &res);
+
+        if res == 0 {
+            self.0 = res;
+            return Turn(p + 1);
+        }
+        if res > SIZE {
+            fin = p + 1;
+        }
+        self.0 = res % SIZE;
+
+        Turn(fin)
+    }
+}
+
+#[aoc(day1, part2)]
+fn solve_part2(input: &Input1) -> u32 {
+    let mut count = 0;
+
+    let w = Wheel::new(50);
+    input.data.iter().fold(w, |mut acc, r| {
+        let res = match r {
+            Move::Left(amount) => acc.left(*amount),
+            Move::Right(amount) => acc.right(*amount),
+        };
+        dbg!(&acc, &res);
+        count += res.val();
+        acc
+    });
+    count
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_sample_1() {
+    fn test_sample_part1() {
         let s = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82\n";
         let input = input_generator(s).unwrap();
 
         assert_eq!(solve_part1(&input), 3);
+    }
+
+    #[test]
+    fn test_sample_part2() {
+        let s = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82\n";
+        let input = input_generator(s).unwrap();
+
+        assert_eq!(solve_part2(&input), 6);
+    }
+
+    #[test]
+    fn test_wheel_initialization() {
+        let wheel = Wheel::new(50);
+        assert_eq!(wheel.pos(), 50);
+    }
+
+    #[test]
+    fn test_wheel_left_movement() {
+        let mut wheel = Wheel::new(50);
+        let turn = wheel.left(30);
+        assert_eq!(wheel.pos(), 20);
+        assert_eq!(turn.val(), 0);
+
+        let turn = wheel.left(120);
+        assert_eq!(wheel.pos(), 0);
+        assert_eq!(turn.val(), 2);
+    }
+
+    #[test]
+    fn test_wheel_right_movement() {
+        let mut wheel = Wheel::new(50);
+        let turn = wheel.right(30);
+        assert_eq!(wheel.pos(), 80);
+        assert_eq!(turn.val(), 0);
+
+        let turn = wheel.right(120);
+        assert_eq!(wheel.pos(), 0);
+        assert_eq!(turn.val(), 2);
+    }
+
+    #[test]
+    fn test_turn_value() {
+        let turn = Turn(42);
+        assert_eq!(turn.val(), 42);
     }
 }
